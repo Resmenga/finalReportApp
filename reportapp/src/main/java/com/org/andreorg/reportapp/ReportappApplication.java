025edce1e68afdcd6561b70b45f0211a6f4410a2
DIOpo <DIOpo@DESKTOP-TP38JOS.home>
2018-06-17 17:10:07 +0200

    maven spring boot and spring MVC base project

---
+++ reportapp/src/main/java/com/org/andreorg/reportapp/ReportappApplication.java
@@ -0,0 +1,12 @@
+package com.org.andreorg.reportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportappApplication.class, args);
+	}
+}
