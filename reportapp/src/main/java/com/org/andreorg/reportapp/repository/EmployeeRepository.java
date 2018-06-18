04250c53880fd36010589643f3aed2e14adfc363
DIOpo <DIOpo@DESKTOP-TP38JOS.home>
2018-06-18 17:05:23 +0200

    Adding search By EmployeeID, FirstName, LastName, Email, Department ID
    and salary

--- reportapp/src/main/java/com/org/andreorg/reportapp/repository/EmployeeRepository.java
+++ reportapp/src/main/java/com/org/andreorg/reportapp/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.org.andreorg.reportapp.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,10 @@ import com.org.andreorg.reportapp.model.Employee;
 @Repository("employeeRepository")
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long>, EmployeeRepositoryCustom {
 	Employee findByEmail(String email);
+	Page<Employee> findByFirstName(Pageable of, String firstName);
+	Page<Employee> findByLastName(Pageable pageable, String lastName);
+	Page<Employee> findByEmail(Pageable pageable, String email);
+	Page<Employee> findByDepartmentID(Pageable pageable, int departmentID);
+	Page<Employee> findByEmpID(Pageable pageable, Long empID);
+	Page<Employee> findBySalary(Pageable pageable, int salary);
 }
