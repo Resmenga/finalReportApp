da6afffa4beafda4899d130cfc052af00ca631f0
DIOpo <DIOpo@DESKTOP-TP38JOS.home>
2018-06-20 00:22:31 +0200

    adding functionality to change input type based on selected option

--- reportapp/src/main/java/com/org/andreorg/reportapp/service/EmployeeService.java
+++ reportapp/src/main/java/com/org/andreorg/reportapp/service/EmployeeService.java
@@ -6,17 +6,4 @@ public interface EmployeeService {
 	public Employee findEmployeeByEmail(String email);
 	public void saveEmployee(Employee employee);
 	public Employee updateEmployee(Employee employee);
-	//TODO: remove this method from here
-	public static boolean isNumeric(String str) {
-	    if (str == null) {
-	        return false;
-	    }
-	    int sz = str.length();
-	    for (int i = 0; i < sz; i++) {
-	        if (Character.isDigit(str.charAt(i)) == false) {
-	            return false;
-	        }
-	    }
-	    return true;
-	}
 }
